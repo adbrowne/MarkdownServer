7d9372e66356dd4ccb6d0597c346ec214a070eab
Andrew Browne <brownie@brownie.com.au>
2012-04-22 20:25:43 +1000

    Added page index to the homepage

--- MarkPadServer/MarkPadServer/Controllers/HomeController.cs
+++ MarkPadServer/MarkPadServer/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MarkPadServer.Controllers
 {
+    using MarkPadServer.PageStore;
+
     public class HomeController : Controller
     {
+        private readonly IPageStore pageStore;
+
+        public HomeController(IPageStore pageStore)
+        {
+            this.pageStore = pageStore;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(pageStore.ListPages());
         }
     }
 }
